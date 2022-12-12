f8647006b2d80feca076ae51efabf0aae87ec0c1
Hillary Muller <hillaryaward@gmail.com>
2022-12-12 11:58:33 -0600

    commit tree 691a35be723996e768ffb68ae48a47bdaf5e5954

--- src/features/quotes/QuoteForm.js
+++ src/features/quotes/QuoteForm.js
@@ -1,21 +1,34 @@
 import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 import { addQuote } from "./quotesSlice";
+import { useDispatch } from 'react-redux';
 
 function QuoteForm() {
+  const dispatch = useDispatch();
+
   const [formData, setFormData] = useState({
     // set up a controlled form with internal state
+    content: "",
+    author: ""
     // look at the form to determine what keys need to go here
   });
 
   function handleChange(event) {
     // Handle Updating Component State
+    setFormData({...formData, [event.target.name]: event.target.value});
   }
 
   function handleSubmit(event) {
+    event.preventDefault();
     // Handle Form Submit event default
+    const quote = {...formData, id: uuid(), votes: 0};
     // Create quote object from state
+    dispatch(addQuote(quote));
     // Pass quote object to action creator
+    setFormData({
+      content: "",
+      author: ""
+    });
     // Update component state to return to default state
   }
 
@@ -25,7 +38,7 @@ function QuoteForm() {
         <div className="col-md-8 col-md-offset-2">
           <div className="panel panel-default">
             <div className="panel-body">
-              <form className="form-horizontal">
+              <form className="form-horizontal" onSubmit={handleSubmit}>
                 <div className="form-group">
                   <label htmlFor="content" className="col-md-4 control-label">
                     Quote
@@ -34,7 +47,9 @@ function QuoteForm() {
                     <textarea
                       className="form-control"
                       id="content"
+                      name="content"
                       value={formData.content}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
@@ -47,13 +62,16 @@ function QuoteForm() {
                       className="form-control"
                       type="text"
                       id="author"
+                      name="author"
                       value={formData.author}
+                      onChange={handleChange}
                     />
                   </div>
                 </div>
                 <div className="form-group">
                   <div className="col-md-6 col-md-offset-4">
                     <button type="submit" className="btn btn-default">
+
                       Add
                     </button>
                   </div>
--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -4,6 +4,70 @@
 // Reducer
 const initialState = [];
 
+export function addQuote(quoteObj) {
+  return ({
+    type: 'quotes/add',
+    payload: quoteObj,
+  });
+}
+export function removeQuote(quoteId) {
+  return ({
+    type: 'quotes/remove',
+    payload: quoteId,
+  });
+}
+
+export function upvoteQuote(quoteId) {
+  return ({
+    type: 'quotes/upvote',
+    payload: quoteId,
+  });
+}
+
+export function downvoteQuote(quoteId) {
+  return ({
+    type: 'quotes/downvote',
+    payload: quoteId,
+  });
+}
+
 export default function quotesReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'quotes/add':
+      return (
+        [...state, action.payload]
+      );
+      case 'quotes/remove':
+        const updatedQuotes = state.filter((quote) => quote.id !== action.payload);
+        return (
+         updatedQuotes
+        );
+        case 'quotes/upvote':
+          return state.map((quote) => {
+            if (quote.id === action.payload) {
+              return {
+                ...quote,
+                votes: quote.votes + 1,
+              };
+            } else {
+              return quote;
+            }
+          });
+          case 'quotes/downvote':
+            return state.map((quote) => {
+              if (quote.id === action.payload && quote.votes > 0) {
+                return {
+                  ...quote,
+                  votes: quote.votes - 1,
+                };
+              } else {
+                return quote;
+              }
+            });
+      default:
+         break;
+  }
   return state;
 }
+
+
